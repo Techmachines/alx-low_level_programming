2caf022f2b60c2c926c9598ed93b9973ddeaafb9
Bussyslim <107619226+Bussyslim@users.noreply.github.com>
2022-07-13 03:02:22 -0700

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _puts - Prints a string
- * @str: The string to print
- *
- * Return: void
- */
-void _puts(char *str)
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
+void print_rev(char *s)
 {
-	for (; *str != '\0'; str++)
+	int c = 0;
+
+	while (s[c] != '\0')
 	{
-		_putchar(*str);
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
 	}
 
 	_putchar('\n');
