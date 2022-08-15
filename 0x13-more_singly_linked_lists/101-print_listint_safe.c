61e683fec283fbaeca34dd622f2ceb66b6791aeb
Bussyslim <107619226+Bussyslim@users.noreply.github.com>
2022-08-15 15:32:29 -0700

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,66 +1,97 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * find_listint_loop_fl - finds a loop in a linked list
- *
- * @head: linked list to search
- *
- * Return: address of node where loop starts/returns, NULL if no loop
+ * count_nodes_till_loop - count nodes to know now many unique nodes to print
+ * @head: pointer to head pointer of linked list
+ * Return: number of unique nodes in list before a loop
  */
-listint_t *find_listint_loop_fl(listint_t *head)
+int count_nodes_till_loop(const listint_t *head)
 {
-	listint_t *ptr, *end;
+	int count = 0;
+	const listint_t *turtle, *hare;
 
-	if (head == NULL)
-		return (NULL);
+	turtle = hare = head;
 
-	for (end = head->next; end != NULL; end = end->next)
+	while (turtle != NULL && hare != NULL)
 	{
-		if (end == end->next)
-			return (end);
-		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
+		turtle = turtle->next;
+		hare = hare->next->next;
+		count++;
+
+		if (turtle == hare)
+		{
+			turtle = head;
+			while (turtle != hare)
+			{
+				turtle = turtle->next;
+				hare = hare->next;
+				count++;
+			}
+			return (count);
+		}
 	}
-	return (NULL);
+	return (0);
 }
 
 /**
- * free_listint_safe - frees a listint list, even if it has a loop
- *
- * @h: head of list
- *
- * Return: number of nodes freed
+ * loop - find if there's a loop in linked list
+ * @head: pointer to head pointer of linked list
+ * Return: 0 if no loop, 1 if loop
  */
-size_t free_listint_safe(listint_t **h)
+int loop(const listint_t *head)
 {
-	listint_t *next, *loopnode;
-	size_t len;
-	int loop = 1;
+	const listint_t *turtle, *hare;
 
-	if (h == NULL || *h == NULL)
-		return (0);
+	turtle = hare = head;
 
-	loopnode = find_listint_loop_fl(*h);
-	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	while (turtle != NULL && hare != NULL)
 	{
-		len++;
-		next = (*h)->next;
-		if (*h == loopnode && loop)
-		{
-			if (loopnode == loopnode->next)
-			{
-				free(*h);
-				break;
-			}
-			len++;
-			next = next->next;
-			free((*h)->next);
-			loop = 0;
-		}
-		free(*h);
+		turtle = turtle->next;
+		hare = hare->next->next;
+
+		if (turtle == hare)
+			return (1);
 	}
-	*h = NULL;
-	return (len);
+	return (0);
+}
+
+/**
+ * print_listint_safe - prints list with addresses
+ * @head: pointer to head pointer of linked list
+ * Return: number of nodes in list, exit(98) if failed
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int count = 0;
+	int loop_found;
+	size_t num_nodes = 0;
+	const listint_t *tmp;
+
+	if (head == NULL)
+		exit(98);
+
+	loop_found = loop(head);
+
+	if (loop_found == 1) /* print upto last node before loop if loop */
+	{
+		count = count_nodes_till_loop(head);
+		for (loop_found = 0; loop_found < count; loop_found++)
+		{
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			num_nodes += 1;
+			tmp = tmp->next;
+		}
+	}
+	else if (loop_found == 0) /* print regularly upto NULL if no loop */
+	{
+		tmp = head;
+		while (tmp != NULL)
+		{
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			num_nodes += 1;
+			tmp = tmp->next;
+		}
+	}
+
+	return (num_nodes);
 }
