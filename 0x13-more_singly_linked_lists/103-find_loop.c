0c5d1bb7901a14d59c50d344433f4e284f24b46c
Bussyslim <107619226+Bussyslim@users.noreply.github.com>
2022-08-15 14:58:11 -0700

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,23 +1,37 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * find_listint_loop - Find the loop inside a list
- * @head: Pointer to the start of a list
- *
- * Return: Address of where the loop starts, NULL if
- * no loop is found
+ * find_listint_loop - find loop in linked list
+ * @head: pointer to head pointer of linked list
+ * Return: address of node where loop starts
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *holder;
+	listint_t *Tom, *Jerry;
 
-	while (head != NULL)
+	/* determine if loop exists by seeing if Tom and Jerry meets */
+	/* start them at head, move Tom one node and Jerry two nodes */
+	Tom = Jerry = head;
+
+	while (Tom != NULL && Jerry != NULL)
 	{
-		holder = head;
-		head = head->next;
-		if (holder < head)
-			return (head);
+		Tom = Tom->next;
+		Jerry = Jerry->next->next;
+
+		if (Tom == Jerry)
+		{
+			/* start Tom at head and Jerry at address they met */
+			/* move Tom and Jerry one node to find loop origin */
+			Tom = head;
+			while (Tom != Jerry)
+			{
+				Tom = Tom->next;
+				Jerry = Jerry->next;
+			}
+			return (Tom);
+		}
 	}
+
 	return (NULL);
 }
