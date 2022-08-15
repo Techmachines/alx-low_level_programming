6b1673b591c3b22fa72caa473a8add2c6884034a
Bussyslim <107619226+Bussyslim@users.noreply.github.com>
2022-08-15 14:25:28 -0700

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,78 +1,71 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_nodeaddress - Add a node to a list with
- * @head: Pointer to the pointer of the start of the list
- * @address: The address of another list's node
- *
- * Return: Address of new node, NULL if it fails
- */
-listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address)
-{
-	listadd_t *new;
+* print_listint_safe - prints a list but safely
+* @head: the head node. pointer
+*
+* Description: dont wanna print a infinite loop
+*	either match addresses or free the head so cant loop back
+* Return: the count number
+* A: we make an array that will store everything from head given
+* B: while the head is not null, we iterate through
+* C: count is initially 0 and i is 0 so we dont even loop through the first
+*	time, we skip this. we assign array to the head and increment head
+*	then we increase count so when we loop through again, we can go
+*	into the for loop
+* D: inside the loop, we check if the head value is equal to the array values
+*	if we found a match then we turn the boolean flag on and break and
+*	we make the index equal to whatever the i counter was
+* E: if we found a match, which is a loop, we break out of the while loop
+* F: new loop, we will loop through our array values we copied over from head
+*	and we print out their pointer address and their value.
+* G: if we had an infinite loop we have a flag check of 1 which will
+*	also print out the index which is the last one.
+*/
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		free_listadd(*head);
-		exit(98);
-	}
-	new->address = (void *)address;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-
-/**
- * free_listadd - Free a list
- * @head: Pointer to the start of the list
- */
-void free_listadd(listadd_t *head)
-{
-	listadd_t *killnode;
-
-	while (head != NULL)
-	{
-		killnode = head;
-		head = head->next;
-		free(killnode);
-	}
-}
-
-/**
- * print_listint_safe - Print out a given list, but only once if it loops
- * @head: Pointer to the start of the list
- *
- * Return: Number of nodes, if it fails print 98
- */
 size_t print_listint_safe(const listint_t *head)
 {
-	listadd_t *newhead;
-	listadd_t *checker;
-	unsigned int count;
+	size_t count = 0;
+	size_t index = 0;
+	listint_t const **array;/* A */
 
-	count = 0;
-	newhead = NULL;
-	while (head != NULL)
+	array = malloc(sizeof(listint_t *) * 1024);
+	if (!array)
+		exit(98);
+	unsigned int i = 0;
+	unsigned int flag = 0;
+
+	while (head != NULL)/* B */
 	{
-		checker = newhead;
-		while (checker != NULL)
+		for (i = 0; i < count; i++)/* C*/
 		{
-			if (head == checker->address)
+			if (head == array[i])/* D */
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listadd(newhead);
-				return (count);
+				flag = 1;
+				index = i;
+				break;
 			}
-			checker = checker->next;
+			else
+				flag = 0;
 		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		add_nodeaddress(&newhead, head);
+
+		if (flag == 1)/* E */
+			break;
+		array[count] = head;
 		head = head->next;
 		count++;
 	}
-	free_listadd(newhead);
+
+	i = 0;
+	while (i < count)/* F */
+	{
+		printf("[%p] %d\n", (void *)array[i], array[i]->n);
+		i++;
+	}
+	if (flag == 1)/* G */
+	{
+		printf("-> [%p] %d\n", (void *)array[index], array[index]->n);
+	}
+	free(array);
 	return (count);
 }
