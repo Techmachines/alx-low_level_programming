e14adb7f12b9cb48e67bc3c0fe7c78c5d2560c30
Bussyslim <107619226+Bussyslim@users.noreply.github.com>
2022-08-15 11:34:48 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,21 +3,21 @@
 #include "lists.h"
 
 /**
- * free_listint2 - function that frees a listint_t list.
- * @head: header pointer
+ * free_listint2 - frees a list but head is null
+ * @head: linked list and head
+ *
  */
-
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
-	listint_t *tmp2;
 
-	tmp2 = *head;
-	while (tmp2 != NULL && head != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		tmp = tmp2;
-		tmp2 = tmp2->next;
-		free(tmp);
+		tmp = *head;
+		free(*head);
+		*head = tmp->next;
 	}
 	*head = NULL;
 }
