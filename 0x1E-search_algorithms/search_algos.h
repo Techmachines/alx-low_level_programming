d3bc19b136b5addc6718a9beceb3c04e5f053430
Ogundimine Aderonke Yetunde <107619226+Bussyslim@users.noreply.github.com>
2023-01-29 15:26:41 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,14 @@
-#ifndef _search_algos_
-#define _search_algos_
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
 
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t l, size_t r);
-int binary_search_index(int *array, size_t l, size_t r, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
-#endif /* _search_algos_ */
+#endif
